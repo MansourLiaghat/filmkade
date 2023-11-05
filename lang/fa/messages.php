696f7f8d92ee67a0b1d113019d98f79f86ce2923
MansourLiaghat <mansourliaghat@yahoo.com>
2023-11-05 18:44:22 +0430

    fix , translate forget password > email

--- lang/fa/messages.php
+++ lang/fa/messages.php
@@ -6,4 +6,5 @@ return [
     'please_check_verified_email' => 'خواهشمند است جهت بهره برداری از تمام امکانات سایت ایمیل خود را تائید بفرمائید .',
     'verification-link-sent' => 'لینک تائیدیه ایمیل ارسال شد .',
     'email_was_verified' => 'ایمیل شما تائید شد .',
+    'reset-password' => 'لینک بازیابی رمز عیور ارسال شد',
 ];
